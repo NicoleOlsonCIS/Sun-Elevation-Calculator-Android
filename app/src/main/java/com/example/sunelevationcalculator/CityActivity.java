d66780c48ee2cea2ac7851160bcecd37c86a75e0
NicoleOlsonCIS <36344090+NicoleOlsonCIS@users.noreply.github.com>
2020-05-31 13:13:51 -0700

    Implement SQL storage via Android Rooms to store user data on location.

--- app/src/main/java/com/example/sunelevationcalculator/CityActivity.java
+++ app/src/main/java/com/example/sunelevationcalculator/CityActivity.java
@@ -1,14 +1,160 @@
 package com.example.sunelevationcalculator;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Calendar;
+
+// selected button color: #4CAF50
+// unselected button color: #9C27B0
+
 public class CityActivity extends AppCompatActivity {
+
+    int userYear;
+    int userMonth;
+    int userDay;
+    int userInterval = 5; // default user interval is 5 min
+    String unselected = "#9C27B0";
+    String selected = "#4CAF50";
+    View fiveMinButton;
+    View fifteenMinButton;
+    View thirtyMinButton;
+    View sixtyMinButton;
+
+    Boolean five = true;
+    Boolean fifeteen = false;
+    Boolean thirty = false;
+    Boolean sixty = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city);
+
+        fiveMinButton = findViewById(R.id.button2);
+        fifteenMinButton = findViewById(R.id.button5);
+        thirtyMinButton = findViewById(R.id.button6);
+        sixtyMinButton = findViewById(R.id.button7);
+
+        // default 5 min button to "selected" and the rest unselected
+        fiveMinButton.setBackgroundColor(Color.parseColor(selected));
+        fifteenMinButton.setBackgroundColor(Color.parseColor(unselected));
+        thirtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+        sixtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+
+        five = true;
+        fifeteen = false;
+        thirty = false;
+        sixty = false;
+
+        System.out.println ("CITY ACTIVITY SCREEN NOW UP");
+        System.out.println ("Storing today's date as selected date");
+
+        Calendar c = Calendar.getInstance();
+        int d = c.get(Calendar.DAY_OF_MONTH);
+        int m = c.get(Calendar.MONTH);
+        int y = c.get(Calendar.YEAR);
+
+        System.out.println ("Today's date is " + d + " " + m + " " + y);
+
+        userYear = y;
+        userMonth = m;
+        userDay = d;
+
+        // from: https://www.semicolonworld.com/question/49188/android-ondatechangedlistener-how-do-you-set-this
+        DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker1);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), new DatePicker.OnDateChangedListener() {
+
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                userYear = year;
+                userMonth = month;
+                userDay = dayOfMonth;
+                System.out.println("New date set! year: " + userYear + " month: " + userMonth + " day: " + userDay); // set a global variable when user changes scroll date
+            }
+        });
+    }
+
+    public void onClick5min(View view)
+    {
+        //System.out.println("5 min button clicked!");
+        if (five == false) { changeButtonState(5); } // nothing happens if you click a button that's already in "selected" state
+    }
+
+    public void onClick15min(View view)
+    {
+        //System.out.println("15 min button clicked!");
+        if (fifeteen == false) { changeButtonState(15); }
+    }
+
+    public void onClick30min(View view)
+    {
+        //System.out.println("30 min button clicked!");
+        if (thirty == false) { changeButtonState(30); }
+    }
+
+    public void onClick60min(View view)
+    {
+        //System.out.println("60 min button clicked!");
+        if (sixty == false) { changeButtonState(60); }
+    }
+
+    private void changeButtonState(int minutesSelected)
+    {
+        System.out.println(minutesSelected + " button selected!");
+
+        if (minutesSelected == 5)
+        {
+            five = true;
+            fifeteen = false;
+            thirty = false;
+            sixty = false;
+            fiveMinButton.setBackgroundColor(Color.parseColor(selected));
+            fifteenMinButton.setBackgroundColor(Color.parseColor(unselected));
+            thirtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+            sixtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+        }
+        else if (minutesSelected == 15)
+        {
+            five = false;
+            fifeteen = true;
+            thirty = false;
+            sixty = false;
+            fiveMinButton.setBackgroundColor(Color.parseColor(unselected));
+            fifteenMinButton.setBackgroundColor(Color.parseColor(selected));
+            thirtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+            sixtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+        }
+        else if (minutesSelected == 30)
+        {
+            five = false;
+            fifeteen = false;
+            thirty = true;
+            sixty = false;
+            fiveMinButton.setBackgroundColor(Color.parseColor(unselected));
+            fifteenMinButton.setBackgroundColor(Color.parseColor(unselected));
+            thirtyMinButton.setBackgroundColor(Color.parseColor(selected));
+            sixtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+        }
+        else if (minutesSelected == 60)
+        {
+            five = false;
+            fifeteen = false;
+            thirty = false;
+            sixty = true;
+            fiveMinButton.setBackgroundColor(Color.parseColor(unselected));
+            fifteenMinButton.setBackgroundColor(Color.parseColor(unselected));
+            thirtyMinButton.setBackgroundColor(Color.parseColor(unselected));
+            sixtyMinButton.setBackgroundColor(Color.parseColor(selected));
+        }
     }
 }
+
--- app/src/main/java/com/example/sunelevationcalculator/MainActivity.java
+++ app/src/main/java/com/example/sunelevationcalculator/MainActivity.java
@@ -1,17 +1,290 @@
 package com.example.sunelevationcalculator;
+//package com.androdocs.currentlocation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.os.AsyncTask;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.room.ColumnInfo;
+import androidx.room.Dao;
+import androidx.room.Database;
+import androidx.room.Delete;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    int PERMISSION_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
+    double latitude = -1000;
+    double longitude = -1000;
+    String userCity = "";
+    String userState = "";
+    String userCountry = "";
+    Boolean userSaved = false;
+
+    TextView latTextView, lonTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int count = 0;
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
+    }
+    
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Granted. Start getting the location information
+                getLastLocation();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLastLocation(){
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if (location == null) {
+                                    requestNewLocationData();
+                                } else {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                    System.out.println("Got user location, latitude " + latitude + " longitude " + longitude);
+                                    parseData();
+                                }
+                            }
+                        }
+                );
+            } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
+        }
+    }
+
+    private void parseData()
+    {
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            //String postalCode = addresses.get(0).getPostalCode();
+            //String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+            //System.out.println(address);
+            userCity = city;
+            userState = state;
+            userCountry = country;
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+        System.out.println("ADDRESSES " + addresses);
+        System.out.println();
+
+        if (userSaved == false) { saveUserLocation(); } // trying to prevent bug of this running multiple times
+    }
+
+    private void saveUserLocation()
+    {
+
+        // Save it to Room database for the user
+        final AppDatabase appDatabase = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
+
+
+        new Thread(new Runnable() // always do db stuff on another thread
+        { @Override public void run()
+        {
+            User user = new User();
+            user.setUserCity(userCity);
+            user.setUserState(userState);
+            user.setUserCountry(userCountry);
+            user.setUserLatitude(latitude);
+            user.setUserLongitude(longitude);
+            appDatabase.userDao().insertUsers (user);
+            userSaved = true;
+        }
+        }).start();
+        //Handler handler= new Handler();
+        //handler.removeCallbacks(runnable); // NEED TO FIGURE OUT HOW TO STOP THREADS : W/System: A resource failed to call close.
+
+        printUser();
+        appDatabase.close();
+        return;
+    }
+
+    private void printUser()
+    {
+        // Save it to Room database for the user
+        final AppDatabase appDatabase = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
+
+        new Thread(new Runnable() // always do db stuff on another thread
+        { @Override public void run()
+        {
+            User users [];
+            users = appDatabase.userDao().loadAllUsers();
+            System.out.println("PRINTING USERS");
+            System.out.println(users);
+
+            if (users.length != 0)
+            {
+                User user = users[0];
+
+                String ucity = user.getUserCity();
+                String uState = user.getUserState();
+                String uCountry = user.getUserCountry();
+                Double uLatitude = user.getUserLatitude();
+                Double uLongitude = user.getUserLongitude();
+
+                System.out.println("User data retrieved from Database: ");
+                System.out.println("City: " + ucity);
+                System.out.println("State: " + uState);
+                System.out.println("Country: " + uCountry);
+                System.out.println("Latitude: " + uLatitude);
+                System.out.println("Longitude: " + uLongitude);
+
+            }
+
+            appDatabase.close();
+
+
+        }
+        }).start();
+        //Handler handler= new Handler();
+        //handler.removeCallbacks(runnable);
+    }
+
+
+    //private void storeAddress(String city, String state, String country)
+    //{
+    //    System.out.println("Storing address!");
+    //}
+
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData(){
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            //latTextView.setText(mLastLocation.getLatitude()+"");
+            //lonTextView.setText(mLastLocation.getLongitude()+"");
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            System.out.println("Got user location, latitude " + latitude + " longitude " + longitude);
+        }
+    };
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
     }
 
     public void onClickName(View view)
@@ -31,3 +304,4 @@ public class MainActivity extends AppCompatActivity {
         // go to Alerts lnading page Activity
     }
 }
+
